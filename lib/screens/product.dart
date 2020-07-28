447c148540e09787fc7090cfdd32cf08ead917c3
mitali457 <mitalimondal450@gmail.com>
2020-07-28 12:40:36 +0530

    firstcommit

---
+++ lib/screens/product.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:furnitureapp/widgets/constant.dart';
+import 'package:furnitureapp/widgets/product_body.dart';
+
+class ProductsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      backgroundColor: kPrimaryColor,
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      centerTitle: false,
+      title: Text(''),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset("assets/icons/notification.svg"),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
